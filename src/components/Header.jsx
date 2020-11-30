e9ef4561852608306cd8d3cc6344aff6758e60bd
Fabian Soosaithasan <fabian.soosaithasan@gmail.com>
2020-11-30 18:43:25 +0100

    Fix borders in navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ import { Link } from 'react-router-dom';
         <Link to="/"><span className="font-semibold text-xl tracking-tight text-gray-800">@fzoozai</span></Link>
       </div>
       <nav className="block">
-        <Link to="/"><span className="inline-block text-gray-800 hover:text-gray-600 mr-3 navitem font-primary bg-primary_light border-double border-4">Home</span></Link>
-        <Link to="/About"><span className="inline-block text-gray-800 hover:text-gray-600 mr-3 navitem font-primary bg-primary_light border-double border-4">About</span></Link>
-        <Link to="/Contact"><span className="inline-block text-gray-800 hover:text-gray-600 mr-3 navitem font-primary bg-primary_light border-double border-4">Contact</span></Link>
+        <Link to="/"><span className="inline-block text-gray-800 hover:text-gray-600 mr-3 navitem font-primary bg-primary_light border-solid border-2 font-bold">Home</span></Link>
+        <Link to="/About"><span className="inline-block text-gray-800 hover:text-gray-600 mr-3 navitem font-primary bg-primary_light border-solid border-2 font-bold">About</span></Link>
+        <Link to="/Contact"><span className="inline-block text-gray-800 hover:text-gray-600 mr-3 navitem font-primary bg-primary_light border-solid border-2 font-bold">Contact</span></Link>
       </nav>
     </div>
   </header>
