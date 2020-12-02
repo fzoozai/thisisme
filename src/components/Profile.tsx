233665d46a8e52dfcf7275937ffd383618de2b05
Fabian Soosaithasan <fabian.soosaithasan@gmail.com>
2020-12-02 19:19:45 +0100

    Create initial draft of the <Profile /> component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import '../css/index.css';
+import Chicago from '../chicago.jpg';
 
 const Profile = () => (
-  <div className="text-center container sm:text-left">
-    <div className="border-2 border-primary rounded-lg m-2 flex-col">
-      <div>Bild</div>
-      <div className="text-pink-200">Bio</div>
+  <div className="text-center sm:text-left">
+    <div className="flex flex-col items-center">
+      <div
+        className="text-indigo-300 bg-green-700 h-72 w-72 bg-ch bg-cover rounded-full mt-4"
+        style={{ backgroundImage: `url(${Chicago})` }}
+      ></div>
+      <div className="text-red-900 mt-6 mb-8 max-w-md text-center px-4">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s.
+      </div>
     </div>
   </div>
 );
