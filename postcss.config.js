ef70691b5b59af4560156c75a46782a15c76a856
Fabian Soosaithasan <fabian.soosaithasan@gmail.com>
2020-12-02 13:35:06 +0100

    Set up PostCSS properly in react project

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
-const tailwindcss = require("tailwindcss");
+const tailwindcss = require('tailwindcss');
 
 module.exports = {
   plugins: [
-    tailwindcss("tailwind.config.js"),
-    require("autoprefixer")
+    tailwindcss('tailwind.config.js'),
+    require('autoprefixer'),
+    require('tailwindcss')
   ]
 };
