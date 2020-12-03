645ba9bcba64770154850c1eda8bd77e3932bbea
Fabian Soosaithasan <fabian.soosaithasan@gmail.com>
2020-12-03 08:36:56 +0100

    Set a limit for the amount of objects to fetch from the github api

--- src/components/GithubRepos.tsx
+++ src/components/GithubRepos.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const GithubRepos = () => <div>GithubRepos</div>;
+interface Repo {
+  id: number;
+  name: string;
+}
+
+const GithubRepos: React.FC = () => {
+  const [repos, setRepos] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        'https://api.github.com/users/fzoozai/repos?per_page=10'
+      );
+      console.log('res', result.data);
+      setRepos(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {repos.map((repo) => (
+        <div key={repo.id}>{repo.name}</div>
+      ))}
+    </div>
+  );
+};
 
 export default GithubRepos;
