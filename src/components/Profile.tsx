83ce84ea0b0d9e303b1bb8774f476752a6503966
Fabian Soosaithasan <fabian.soosaithasan@gmail.com>
2020-12-03 08:54:36 +0100

    Update css color class for <Profile /> component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Chicago from '../chicago.jpg';
 
 const Profile = () => (
@@ -8,7 +7,7 @@ const Profile = () => (
         className="text-indigo-300 h-72 w-72 bg-ch bg-cover rounded-full mt-4"
         style={{ backgroundImage: `url(${Chicago})` }}
       ></div>
-      <div className="text-red-900 mt-6 mb-8 max-w-md text-center px-4">
+      <div className="text-primary-900 mt-6 mb-8 max-w-md text-center px-4">
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s.
